840a924ee8773ff7702d7b9589cc8e6689cf9700
ugurb96 <ugur.bas096@gmail.com>
2021-10-08 18:53:54 +0300

    HamburgerWithMedia

---
+++ src/app/general-nav/general-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-general-nav',
+  templateUrl: './general-nav.component.html',
+  styleUrls: ['./general-nav.component.css']
+})
+export class GeneralNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
